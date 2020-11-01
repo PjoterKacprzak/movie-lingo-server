9edaf24ab58a9ddbd1e82f733c52bd662f02c3b3
PjoterKacprzak <piotr.kacprzak01@gmail.com>
2020-11-01 12:41:25 +0100

    Initial commit

---
+++ src/test/java/com/example/movielingo/MovieLingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movielingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
